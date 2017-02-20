da6a7bbb2d0234b79879f508492c298356641dee
Nacon <lager_91@hotmail.com>
2017-02-20 09:40:47 +0100

    added readme and gui

--- Snacka/Snacka/Form1.cs
+++ Snacka/Snacka/Form1.cs
@@ -16,5 +16,10 @@ namespace Snacka
         {
             InitializeComponent();
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
