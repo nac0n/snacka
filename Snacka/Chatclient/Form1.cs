2737bad8de8523c2127e5aba6c57dea3c22aff97
Jessica <je3ssica@gmail.com>
2017-03-01 14:01:05 +0100

    send message on enter

--- Snacka/Chatclient/Form1.cs
+++ Snacka/Chatclient/Form1.cs
@@ -137,5 +137,15 @@ namespace Chatclient
            
         }
 
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                if (!textBox1.AcceptsReturn)
+                {
+                    button1.PerformClick();
+                }
+            }
+        }
     }
 }
