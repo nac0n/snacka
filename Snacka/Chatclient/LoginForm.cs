9e9739e5b1a23c21ad3e342a50ae4b315d600c10
Jessica <je3ssica@gmail.com>
2017-03-14 20:32:38 +0100

    first commit on password, needs to be hashed

---
+++ Snacka/Chatclient/LoginForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chatclient
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Jessica\snacka\db\snackaDb.mdf;Integrated Security=True;Connect Timeout=30");
+            string query = "SELECT * from login Where id=1 and password = '"+ textBox2.Text.Trim() + "'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, conn);
+            DataTable dtbl = new DataTable();
+            sda.Fill(dtbl);
+            if(dtbl.Rows.Count == 1)
+            {
+                Form1 chat = new Form1();
+                this.Hide();
+                chat.Show();
+            }
+            else
+            {
+                MessageBox.Show("Wrong Password, try again");
+            }
+
+        }
+    }
+}
--- Snacka/Chatclient/Program.cs
+++ Snacka/Chatclient/Program.cs
@@ -16,7 +16,8 @@ namespace Chatclient
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
+            //Application.Run(new Form1());
         }
     }
 }
