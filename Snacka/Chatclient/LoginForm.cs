f39f81fe21e8cb3c87cbe1a672068f671279b6d4
Jessica <je3ssica@gmail.com>
2017-03-15 13:46:17 +0100

    maybe fixed path to db

--- Snacka/Chatclient/LoginForm.cs
+++ Snacka/Chatclient/LoginForm.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -34,13 +37,18 @@ namespace Chatclient
             }
             return Convert.ToBase64String(hashBytes);
         }
+     
 
         private void Login_Click(object sender, EventArgs e)
         {
             string password = textBox2.Text.Trim();
             string hashed = Hash(password);
+            var path = new DirectoryInfo(Environment.CurrentDirectory).Parent.Parent.Parent.Parent.FullName;
+            Console.WriteLine("****"+path);
+            path = path + @"\db\snackaDb.mdf";
 
-            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Jessica\snacka\db\snackaDb.mdf;Integrated Security=True;Connect Timeout=30");
+            //C:\Users\Jessica\snacka\db\snackaDb.mdf
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="+path+";Integrated Security=True;Connect Timeout=30");
             string query = "SELECT * from login Where username = '" + textBox1.Text.Trim() + "' and password = '"+ hashed + "'";
             SqlDataAdapter sda = new SqlDataAdapter(query, conn);
             DataTable dtbl = new DataTable();
